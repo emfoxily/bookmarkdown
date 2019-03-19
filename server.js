4b28a68e8d8f40904ca57c7c9588821ddf1949fc
emily reno <emilyreno@emilys-MacBook-Pro.local>
2019-03-18 21:15:26 -0400

    fixing mongo issue

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ app.use(express.static('public'))
 
 const bookmarkController = require('./controllers/bookmarks.js')
 
+const uri = 'mongodb://emily:VBhQaVFYwHhecq4@ds117846.mlab.com:17846/heroku_2m3d047j' || 'mongodb://localhost:27017/bookmrkdown'
+
 app.use('/bookmarks', bookmarkController)
 
-mongoose.connect('mongodb://localhost:27017/bookmrkdown', {
-    useNewUrlParser: true })
-
+mongoose.connect(uri, {
+    useNewUrlParser: true 
+})
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose!')
 })
